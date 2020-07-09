e72622faf88099a3af858c68a3129fca58be899a
Navanjane <nkindelpitiya@gmail.com>
2020-07-09 22:54:08 +0530

    admin page

---
+++ admin.php
@@ -0,0 +1,12 @@
+<?php include('inc/header.php')?>
+
+<div class="row" style="margin-top:60px;">
+    <div class="col-md-8"></div>
+    <div class="col-md-4">
+    <button type="button" class="btn btn-outline-secondary">Student List</button>
+    <button type="button" class="btn btn-outline-secondary">Teachers List</button>
+    </div>
+</div>
+
+
+<?php include('inc/footer.php')?>
