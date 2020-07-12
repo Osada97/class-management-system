3b478fee9429fed1288bfb2042bb356854be0467
Navanjane <nkindelpitiya@gmail.com>
2020-07-12 09:19:36 +0530

    teacher compnents added

---
+++ teacher/teacher_dashboard.php
@@ -0,0 +1,34 @@
+<?php include('./teacher_header.php') ?>
+<div class="container-fluid">
+    <h1 class="mt-4">Dashboard</h1>
+</div>
+
+
+    <?php include('./teacher_footer.php') ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
