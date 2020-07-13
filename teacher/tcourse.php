96aaef538cbbb7fc0f1eb2099e8ba2f8c62f63cb
Navanjane <nkindelpitiya@gmail.com>
2020-07-13 21:43:46 +0530

    Teacher_id removed form form

--- teacher/tcourse.php
+++ teacher/tcourse.php
@@ -11,12 +11,7 @@
             <input type="text" class="form-control" id="inputEmail3">
         </div>
     </div>
-    <div class="form-group row">
-        <label for="inputPassword3" class="col-sm-2 col-form-label">Teacher Id</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputPassword3">
-        </div>
-    </div>
+
     <div class="form-group row">
         <label for="inputPassword3" class="col-sm-2 col-form-label">Enroll Key</label>
         <div class="col-sm-10">
