bf95e1eea5899988eb37ba41928a5655a1a9af22
Navanjane <nkindelpitiya@gmail.com>
2020-07-14 08:54:21 +0530

    teacher course dashboard edited

--- teacher/teacher_dashboard.php
+++ teacher/teacher_dashboard.php
@@ -4,6 +4,43 @@
 </div>
 <?php include_once 'tnav.php';?>
 
+<div class="container mt-4">
+    <div class="card-deck">
+        <div class="card">
+            <img src="../img/python.jpg" class="card-img-top" alt="..." style="height: 250px;">
+            <div class="card-body">
+                <h5 class="card-title">Python</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+        <div class="card">
+            <img src="../img/javascript.png" class="card-img-top" alt="..." style="height: 250px;">
+            <div class="card-body">
+                <h5 class="card-title">Javascript</h5>
+                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+        <div class="card">
+            <img src="../img/css.png" class="card-img-top" alt="..." style="height: 250px;">
+            <div class="card-body">
+                <h5 class="card-title">css</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
 <?php include('./teacher_footer.php') ?>
 
 
--- teacher/teacher_header.php
+++ teacher/teacher_header.php
@@ -52,7 +52,7 @@
     <div class="bg-light border-right" id="sidebar-wrapper">
       <div class="sidebar-heading">Teacher's Area</div>
       <div class="list-group list-group-flush">
-        <a href="" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+        <a href="teacher_dashboard.php" class="list-group-item list-group-item-action bg-light">Dashboard</a>
         <a href="tcourse.php" class="list-group-item list-group-item-action bg-light">Manage Courses</a>
         <a href="tstudent.php" class="list-group-item list-group-item-action bg-light">Manage Students</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
