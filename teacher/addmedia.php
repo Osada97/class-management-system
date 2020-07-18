5f68557249470d800cace42460b215a6783ac87b
Navanjane <nkindelpitiya@gmail.com>
2020-07-18 17:11:28 +0530

    addmedia.php added

---
+++ teacher/addmedia.php
@@ -0,0 +1,63 @@
+<?php include_once("../inc/header.php"); ?>
+<?php require_once("../inc/connection.php"); ?>
+
+
+<div class="container-fluid mt-4">
+    <div class="col-md-2"></div>
+    <div class="row justify-content-center">
+    <div class="col-md-8">
+            <div class="jumbotron">
+                <h1 class="display-4">Welcome to Upload Center!</h1>
+                <p class="lead">Upload your Course Material in here</p>
+                <hr class="my-4">
+                <p>Compatible with PDF and other Video formats</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Return to Dashboard</a>
+                </p>
+            </div>
+        <div class="row">
+        <div class="col-md-3">
+            <div class="card bg-light mb-3" style="max-width: 18rem;">
+                <div class="card-header">Students</div>
+                <div class="card-body">
+                    <div class="scroll">
+                        sfkcjgasbukcjbvaskuvjboikvjbsdkvjbsilvkjbwsdkjbvkj
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+            <div class="col-md-9">
+                <form action="" >
+                    <div class="form-group">
+                        <label class="btn btn-success btn-lg btn-block form-control-file">Add Media
+                        <input  type="file" hidden>
+                        </label>
+                    </div>
+                </form>
+
+                <div class="card">
+                    <div class="card-header">
+                        Quote
+                    </div>
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    </div>
+    <div class="col-md-2">
+
+    </div>
+
+</div>
+
+
