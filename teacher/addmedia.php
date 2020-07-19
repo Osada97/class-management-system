ce9cac9da9dd766f2a5b5e7502a6e1be5f6031d0
Navanjane <nkindelpitiya@gmail.com>
2020-07-19 22:18:44 +0530

    Jquery error solved with cdn Update

--- teacher/addmedia.php
+++ teacher/addmedia.php
@@ -454,11 +454,14 @@
     </div>
 </div> -->
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<?php include_once ('../inc/footer.php'); ?>
+
 <script>
     $('#add').click(function(){
         $('#mainme').slideToggle(600);
     });
 </script>
 
-<?php include_once ("teacher_footer.php"); ?>
+
+
+
