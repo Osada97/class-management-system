220d747058a037287ebea1d2471476da03108116
osada97 <osadamanohara55@gmail.com>
2020-07-19 16:49:32 +0530

    Add media Folder

---
+++ course_media/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>JOB HUNTING SYSTEM</title>
+	<link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+
+	<header class="clearfix">
+
+		<div class="logo">
+			<img src="imj/logo1.png" alt="LOGO">
+		</div>
+		<div class="menu">
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+		</div>
+		<div class="but">
+			<button><a href="#">LOG IN</a></button>
+			<button><a href="#">SIGN UP</a></button>
+			<button><a href="#">COMPANY</a></button>
+		</div>
+	</header>
+	
+</body>
+</html>
