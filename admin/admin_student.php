994f0b1acaae4382d86dfd66ddf907634a96e778
Navanjane <nkindelpitiya@gmail.com>
2020-07-26 10:57:18 +0530

    Teacher and Student admin table created

--- admin/admin_student.php
+++ admin/admin_student.php
@@ -1,4 +1,12 @@
 <?php include('../inc/admin_header.php') ?>
+<?php require_once ('../inc/connection.php');
+
+$query = 'select * from student';
+$result = mysqli_query($connection,$query);
+$data = mysqli_fetch_all($result,MYSQLI_ASSOC);
+//var_dump($data);
+
+?>
       <div class="container-fluid">
         <h1 class="mt-4">Student Management</h1>
        
@@ -24,7 +32,43 @@
                     </ul>
                 </div>
 
+    <!--Student Table-->
 
+                <table class="table table-hover mt-4">
+                    <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">User Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col"></th>
+                        <th scope="col">Options</th>
+                        <th scope="col"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach ($data as $details): ?>
+                        <tr>
+                            <td scope="row"><?php echo $details['st_id'];?></td>
+                            <td><?php echo $details['first_name'];?></td>
+                            <td><?php echo $details['last_name'];?></td>
+                            <td><?php echo $details['user_name'];?></td>
+                            <td><?php echo $details['email'];?></td>
+                            <td><button class="btn btn-danger">Remove</button></td>
+                            <td><button class="btn btn-warning">Freeze</button></td>
+                            <td><button class="btn btn-info">More</button></td>
+
+                        </tr>
+                    <?php endforeach;?>
+
+
+                    </tbody>
+                </table>
+
+
+
+                <!--end Table-->
                 <div class="col-md-2"></div>
             </div>
         </div>
--- admin/admin_teacher.php
+++ admin/admin_teacher.php
@@ -5,6 +5,53 @@
       </div>
 
 <?php include_once "../inc/adnav.php"; ?>
+<?php require_once ('../inc/connection.php');
+
+$query = 'select * from teacher';
+$result = mysqli_query($connection,$query);
+$data = mysqli_fetch_all($result,MYSQLI_ASSOC);
+//var_dump($data);
+
+?>
+<!-- start Table -->
+<table class="table table-hover mt-4">
+    <thead>
+    <tr>
+        <th scope="col">id</th>
+        <th scope="col">First Name</th>
+        <th scope="col">Last Name</th>
+        <th scope="col">Skills</th>
+        <th scope="col">Email</th>
+        <th scope="col"></th>
+        <th scope="col">Options</th>
+        <th scope="col"></th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($data as $details): ?>
+        <tr>
+            <td scope="row"><?php echo $details['teacher_id'];?></td>
+            <td><?php echo $details['first_name'];?></td>
+            <td><?php echo $details['last_name'];?></td>
+            <td><?php echo $details['skills'];?></td>
+            <td><?php echo $details['email'];?></td>
+            <td><button class="btn btn-danger">Remove</button></td>
+            <td><button class="btn btn-warning">Freeze</button></td>
+            <td><button class="btn btn-info">More</button></td>
+
+        </tr>
+    <?php endforeach;?>
+
+
+    </tbody>
+</table>
+
+
+
+<!--end Table-->
+
+
+
       
 <?php include_once('../inc/admin_footer.php')?>
 
