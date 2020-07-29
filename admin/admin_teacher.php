eaf0217376e99e4feb16b4c4a637d050f98cc2b2
Navanjane <nkindelpitiya@gmail.com>
2020-07-29 19:54:43 +0530

    modal added

--- admin/admin_teacher.php
+++ admin/admin_teacher.php
@@ -76,6 +76,34 @@
     </form>
 </div>
 
+
+
+<!-- Starting Modal replace id with suitable name and write the button -->
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<!-- Ending Model -->
+
 <!-- start Table -->
 <table class="table table-hover mt-4">
     <thead>
